e1e92df18d3604ac0fc0b8566aedbcc57d498588
Leveton <mleveton@gmail.com>
2015-03-02 10:50:19 -0500

    Initial Commit

---
+++ MELCoda/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MELCoda
+//
+//  Created by Mike Leveton on 3/2/15.
+//  Copyright (c) 2015 MEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MELCoda/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MELCoda
+//
+//  Created by Mike Leveton on 3/2/15.
+//  Copyright (c) 2015 MEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
