c713bc4f792641272b912a125e773ceefcc82733
Leveton <mleveton@gmail.com>
2015-03-24 18:29:37 -0400

    fix for issue #1

--- MELCoda/hideSections.js
+++ MELCoda/hideSections.js
@@ -1,3 +1,4 @@
 var styleTag = document.createElement("style");
-styleTag.textContent = 'div#sidebar, .after-post.widget-area {display:none;}';
+//styleTag.textContent = 'div#sidebar, .after-post.widget-area {display:none;}';
+styleTag.textContent = 'div#sidebar, .footer-widgets {display:none;}';
 document.documentElement.appendChild(styleTag);
